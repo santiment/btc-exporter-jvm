bf9ae6f9464bcf8ed4fc6b3752de4b38a740a5c1
tsanko@gmail.com <tsanko@gmail.com>
2018-07-24 11:49:05 +0300

    Add zookeeper stats

--- src/main/scala/net/santiment/Store.scala
+++ src/main/scala/net/santiment/Store.scala
@@ -2,6 +2,7 @@ package net.santiment
 
 import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectInputStream, ObjectOutputStream}
 import java.nio.ByteBuffer
+import java.util.{Timer, TimerTask}
 
 import com.typesafe.scalalogging.LazyLogging
 import org.apache.curator.framework.{CuratorFramework, CuratorFrameworkFactory}
@@ -31,30 +32,100 @@ trait Store[T] {
   }
 }
 
+case class ZookeeperStats
+(
+  var getData: Long = 0L,
+  var getDataTime: Long = 0L,
+  var setData: Long = 0L,
+  var setDataTime: Long = 0L,
+  var create: Long = 0L,
+  var createTime: Long = 0L,
+  var delete: Long = 0L,
+  var deleteTime: Long = 0L
+) {
+  def minus(other: ZookeeperStats): ZookeeperStats = ZookeeperStats(
+    getData - other.getData,
+    getDataTime - other.getDataTime,
+    setData - other.setData,
+    setDataTime - other.setDataTime,
+    create - other.create,
+    createTime - other.createTime,
+    delete - other.delete,
+    deleteTime - other.deleteTime
+  )
+
+  override def toString: String = {
+    val totalTime = getDataTime + setDataTime + createTime + deleteTime
+    s"ZookeeperStats(getData=$getData, getDataTime=$getDataTime, setData=$setData, setDataTime=$setDataTime, create=$create, createTime=$createTime, delete=$delete, deleteTime=$deleteTime, totalTime=$totalTime)"
+  }
+}
+
+object ZookeeperStats extends LazyLogging {
+  val stats = ZookeeperStats()
+
+  val timer = new Timer()
+
+  val task: TimerTask = new TimerTask {
+
+    var oldStats = ZookeeperStats()
+    override def run(): Unit = {
+      val diff = stats.minus(oldStats)
+      logger.info(s"Zookeeper stats: $diff")
+      oldStats = stats.copy()
+    }
+  }
+
+  timer.scheduleAtFixedRate(task,60000,60000)
+}
+
 class ZookeeperStore[T](private val client:CuratorFramework, path:String)(implicit serde:Serde[T])
   extends Store[T]
     with LazyLogging {
 
+  import ZookeeperStats.stats
+
   override def read: Option[T] = {
-    if (client.checkExists.forPath(path) != null) {
+
+    val start = System.nanoTime()
+    val result = if (client.checkExists.forPath(path) != null) {
       val bytes = client.getData.forPath(path)
       Some(serde.fromByteArray(bytes))
     }
     else {
       None
     }
+
+    stats.getData += 1
+    stats.getDataTime += (System.nanoTime() - start)
+
+    result
   }
 
   override def update(value:T): Unit = {
+    val start = System.nanoTime()
+
     client.setData().forPath(path, serde.toByteArray(value))
+
+    stats.setData += 1
+    stats.setDataTime += (System.nanoTime() - start)
   }
 
   override def create(value:T): Unit = {
+    val start = System.nanoTime()
+
     client.create().creatingParentsIfNeeded().forPath(path, serde.toByteArray(value))
+
+    stats.create += 1
+    stats.createTime += (System.nanoTime() - start)
   }
 
   override def delete(): Unit = {
+    val start = System.nanoTime()
+
     client.delete().deletingChildrenIfNeeded().forPath(path)
+
+    stats.delete += 1
+    stats.deleteTime += (System.nanoTime() - start)
   }
 }
 
